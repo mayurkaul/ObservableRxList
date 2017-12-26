0ff0ae5e3a63d7d7277456b5de4d24c4920590c7
Mayur Kaul <mayur.kaul@ril.com>
2017-12-26 17:42:09 +0530

    Initial commit

---
+++ library/rxobservablelist/src/main/java/com/github/mayurkaul/rxobservablelist/RxBaseObject.kt
@@ -0,0 +1,29 @@
+package com.ril.jio.jioboardsdk.system
+
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+
+internal abstract class RxBaseObject<T> {
+
+    private var mStatusObservable: BehaviorSubject<STATUS> = BehaviorSubject.create<STATUS>();
+
+    abstract fun setKey(key: T)
+
+    abstract fun getKey(): T
+
+    fun setStatus(status:STATUS)
+    {
+        mStatusObservable.onNext(status);
+    }
+
+    fun getStatusObservable():Subject<STATUS>
+    {
+        return mStatusObservable;
+    }
+
+    enum class STATUS
+    {
+        ADDED,UPDATED,REMOVED
+    }
+
+}
